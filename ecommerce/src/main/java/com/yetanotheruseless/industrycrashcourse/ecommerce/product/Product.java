4635f193fb8acdab3e2d795f5fab87a708322b95
Jake <jmannix@salesforce.com>
2021-03-23 13:19:06 -0700

    fix build

--- ecommerce/src/main/java/com/yetanotheruseless/industrycrashcourse/ecommerce/product/Product.java
+++ ecommerce/src/main/java/com/yetanotheruseless/industrycrashcourse/ecommerce/product/Product.java
@@ -10,10 +10,10 @@ import java.util.List;
 
 @Entity
 @Table
-@Getter
-@Setter
-@NoArgsConstructor
-@ToString
+//@Getter
+//@Setter
+//@NoArgsConstructor
+//@ToString
 public class Product {
     @Id
     @SequenceGenerator(name = "product_sequence", sequenceName = "product_sequence", allocationSize = 1)
@@ -35,6 +35,8 @@ public class Product {
     private Double customerReviewAverage;
     @ElementCollection
     private List<String> frequentlyPurchasedWith;
+
+    public Product() {}
 
     public Product(Long id,
                    String sku,
@@ -100,5 +102,155 @@ public class Product {
         this.customerReviewCount = customerReviewCount;
         this.customerReviewAverage = customerReviewAverage;
         this.frequentlyPurchasedWith = frequentlyPurchasedWith;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSku() {
+        return sku;
+    }
+
+    public void setSku(String sku) {
+        this.sku = sku;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Double getRegularPrice() {
+        return regularPrice;
+    }
+
+    public void setRegularPrice(Double regularPrice) {
+        this.regularPrice = regularPrice;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getMediumImage() {
+        return mediumImage;
+    }
+
+    public void setMediumImage(String mediumImage) {
+        this.mediumImage = mediumImage;
+    }
+
+    public String getThumbnailImage() {
+        return thumbnailImage;
+    }
+
+    public void setThumbnailImage(String thumbnailImage) {
+        this.thumbnailImage = thumbnailImage;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public String getLongDescription() {
+        return longDescription;
+    }
+
+    public void setLongDescription(String longDescription) {
+        this.longDescription = longDescription;
+    }
+
+    public Integer getSalesRankShortTerm() {
+        return salesRankShortTerm;
+    }
+
+    public void setSalesRankShortTerm(Integer salesRankShortTerm) {
+        this.salesRankShortTerm = salesRankShortTerm;
+    }
+
+    public Integer getSalesRankMediumTerm() {
+        return salesRankMediumTerm;
+    }
+
+    public void setSalesRankMediumTerm(Integer salesRankMediumTerm) {
+        this.salesRankMediumTerm = salesRankMediumTerm;
+    }
+
+    public Integer getSalesRankLongTerm() {
+        return salesRankLongTerm;
+    }
+
+    public void setSalesRankLongTerm(Integer salesRankLongTerm) {
+        this.salesRankLongTerm = salesRankLongTerm;
+    }
+
+    public Integer getCustomerReviewCount() {
+        return customerReviewCount;
+    }
+
+    public void setCustomerReviewCount(Integer customerReviewCount) {
+        this.customerReviewCount = customerReviewCount;
+    }
+
+    public Double getCustomerReviewAverage() {
+        return customerReviewAverage;
+    }
+
+    public void setCustomerReviewAverage(Double customerReviewAverage) {
+        this.customerReviewAverage = customerReviewAverage;
+    }
+
+    public List<String> getFrequentlyPurchasedWith() {
+        return frequentlyPurchasedWith;
+    }
+
+    public void setFrequentlyPurchasedWith(List<String> frequentlyPurchasedWith) {
+        this.frequentlyPurchasedWith = frequentlyPurchasedWith;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", sku='" + sku + '\'' +
+                ", name='" + name + '\'' +
+                ", type='" + type + '\'' +
+                ", regularPrice=" + regularPrice +
+                ", image='" + image + '\'' +
+                ", mediumImage='" + mediumImage + '\'' +
+                ", thumbnailImage='" + thumbnailImage + '\'' +
+                ", shortDescription='" + shortDescription + '\'' +
+                ", longDescription='" + longDescription + '\'' +
+                ", salesRankShortTerm=" + salesRankShortTerm +
+                ", salesRankMediumTerm=" + salesRankMediumTerm +
+                ", salesRankLongTerm=" + salesRankLongTerm +
+                ", customerReviewCount=" + customerReviewCount +
+                ", customerReviewAverage=" + customerReviewAverage +
+                ", frequentlyPurchasedWith=" + frequentlyPurchasedWith +
+                '}';
     }
 }
--- ecommerce/src/main/java/com/yetanotheruseless/industrycrashcourse/ecommerce/product/ProductService.java
+++ ecommerce/src/main/java/com/yetanotheruseless/industrycrashcourse/ecommerce/product/ProductService.java
@@ -1,6 +1,5 @@
 package com.yetanotheruseless.industrycrashcourse.ecommerce.product;
 
-import org.assertj.core.util.Lists;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -29,28 +28,5 @@ public class ProductService {
     public Long addProduct(Product product) {
         Product saved = productRepository.save(product);
         return saved.getId();
-    }
-
-    public List<Product> list() {
-        return Arrays.asList(
-                new Product(
-                        1L,
-                        "2570",
-                        "Fuji - 110 Color Print Disc Film (24 exposures)",
-                        "HardGood",
-                        1.89,
-                        "http://images.bestbuy.com/BestBuy_US/images/products/2570/2570_rc.jpg",
-                        "http://images.bestbuy.com/BestBuy_US/images/products/2570/2570fp.gif",
-                        "http://images.bestbuy.com/BestBuy_US/images/products/2570/2570_r.gif",
-                        "110 disc color print, 200-speed film; 24 exposures; for use indoors with flash or outdoors",
-                        "110 disc color print, 200-speed film. 24 exposures. For use indoors with flash or outdoors.",
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        Lists.emptyList()
-                )
-        );
     }
 }
