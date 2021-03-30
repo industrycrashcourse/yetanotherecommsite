49711d228f75a77148ba26752f41915465a1b98d
tylerastraub <tyler.straub@yotes.collegeofidaho.edu>
2021-03-29 22:55:00 -0600

    fixed test to compile properly for non-products

--- ecommerce/src/test/java/com/yetanotheruseless/industrycrashcourse/ecommerce/EcommerceApiTests.java
+++ ecommerce/src/test/java/com/yetanotheruseless/industrycrashcourse/ecommerce/EcommerceApiTests.java
@@ -25,7 +25,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ContextConfiguration(classes=EcommerceApplication.class)
-@WebMvcTest(ProductController.class)
+@WebMvcTest
 public class EcommerceApiTests {
 
     @Autowired
