cb0bf34ffe1492ff652fd3c9460225d386010211
MattSamm <46332371+MattSamm@users.noreply.github.com>
2021-04-02 23:58:20 -0600

    Revert "Merge remote-tracking branch 'origin/main' into main"
    This reverts commit 9a4c962869f775c05b2db1cbc06462afed80f6e9, reversing
    changes made to 678c54935963b246ea1397bd8b3397eaa7b423b7.

--- ecommerce/src/main/java/com/yetanotheruseless/industrycrashcourse/ecommerce/product/ProductController.java
+++ ecommerce/src/main/java/com/yetanotheruseless/industrycrashcourse/ecommerce/product/ProductController.java
@@ -5,8 +5,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-
-
 @RestController
 @RequestMapping(path = "api/v1/product")
 public class ProductController {
@@ -18,27 +16,18 @@ public class ProductController {
         this.productService = productService;
     }
 
-    @GetMapping(path = "list")
+    @GetMapping(path = "all")
     public List<Product> list() {
         return productService.findAll();
     }
 
-    @GetMapping(path = "{sku}")
+    @GetMapping("{sku}")
     public Product get(@PathVariable String sku) {
-        return productService.findBySku(sku).orElseThrow(() -> new RuntimeException("dude, not found"));
+        return productService.findBySku(sku).orElseThrow(() -> new RuntimeException("not found"));
     }
 
     @PostMapping
     public Long addProduct(@RequestBody Product product) {
-        if (product.getName() == null) {
-            // no way!  throw exception
-        }
         return productService.addProduct(product);
-    }
-
-
-    @GetMapping(path = "byCustomerReviewAverage/{min}/{max}")
-    public List<Product> byCustomerReviewAverage(@PathVariable Double min, @PathVariable Double max) {
-        return productService.findByCustomerReviewAverageRange(min, max);
     }
 }
