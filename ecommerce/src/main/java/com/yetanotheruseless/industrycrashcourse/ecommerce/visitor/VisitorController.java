c8e3a656483fdc1e39c0033729c1c756e26fcdfa
Lamine Keita <lamine.keita@yotes.collegeofidaho.edu>
2021-04-05 01:27:40 -0600

    Removed shoppingCart implementation, and fixed some errors

---
+++ ecommerce/src/main/java/com/yetanotheruseless/industrycrashcourse/ecommerce/visitor/VisitorController.java
@@ -0,0 +1,35 @@
+package com.yetanotheruseless.industrycrashcourse.ecommerce.visitor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/visitor")
+
+public class VisitorController {
+
+    private final VisitorService visitorService;
+
+    @Autowired
+    public VisitorController(VisitorService visitorService) {
+        this.visitorService = visitorService;
+    }
+
+
+    @GetMapping(path = "list")
+    public List<Visitor> list() {
+        return visitorService.findAll();
+    }
+
+    @GetMapping(path = "{id}")
+    public Visitor get(@PathVariable long id) {
+        return visitorService.findById(id).orElseThrow(() -> new RuntimeException("Lamine did something wrong"));
+    }
+
+    @PostMapping
+    public Long addVisitor(@RequestBody Visitor visitor) {
+        if (visitor.getFirstNameName() == null) {}
+        return visitorService.addVisitor(visitor);
+    }
+}
--- ecommerce/src/test/java/com/yetanotheruseless/industrycrashcourse/ecommerce/EcommerceApiTests.java
+++ ecommerce/src/test/java/com/yetanotheruseless/industrycrashcourse/ecommerce/EcommerceApiTests.java
@@ -3,6 +3,9 @@ package com.yetanotheruseless.industrycrashcourse.ecommerce;
 import com.yetanotheruseless.industrycrashcourse.ecommerce.product.Product;
 import com.yetanotheruseless.industrycrashcourse.ecommerce.product.ProductController;
 import com.yetanotheruseless.industrycrashcourse.ecommerce.product.ProductService;
+import com.yetanotheruseless.industrycrashcourse.ecommerce.visitor.Visitor;
+import com.yetanotheruseless.industrycrashcourse.ecommerce.visitor.VisitorController;
+import com.yetanotheruseless.industrycrashcourse.ecommerce.visitor.VisitorService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -11,8 +14,11 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Optional;
 
 import static org.hamcrest.core.StringContains.containsString;
+import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -28,17 +34,61 @@ public class EcommerceApiTests {
 
     @MockBean
     private ProductService productService;
-
+    private VisitorService visitorService;
     @Test
     public void listAllTest() throws Exception {
         Product dummyFuji = new Product();
         dummyFuji.setName("Fuji");
-        when(productService.findAll()).thenReturn(Arrays.asList(dummyFuji));
+        dummyFuji.setSku("1234");
+        when(productService.findAll()).thenReturn(Collections.singletonList(dummyFuji));
 
         mockMvc
-                .perform(get("/api/v1/product/all"))
+                .perform(get("/api/v1/product/list"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Fuji")));
+
+        reset(productService);
+
+        when(productService.findBySku("1234")).thenReturn(Optional.of(dummyFuji));
+
+        mockMvc
+                .perform(get("/api/v1/product/1234"))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().string(containsString("Fuji")));
     }
+
+    @Test
+    public void incorrectPathTest() throws Exception {
+        mockMvc
+                .perform(get("/api/v1/product/foo/bar/baz"))
+                .andDo(print())
+                .andExpect(status().isNotFound());
+        // .andExpect(content().string(containsString("dude")));
+    }
+
+    @Test
+    public void testVisitor() throws Exception {
+        Visitor visitor = new Visitor();
+        visitor.setId(12345L);
+        visitor.setSurname("Keita");
+        when(visitorService.findAll()).thenReturn(Collections.singletonList(visitor));
+        mockMvc
+                .perform(get("/api/v1/visitor/list"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Keita")));
+
+        reset(visitorService);
+
+        when(visitorService.findById(12345L)).thenReturn(Optional.of(visitor));
+
+        mockMvc
+                .perform(get("/api/v1/visitor/12345"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Keita")));
+    }
+
 }
