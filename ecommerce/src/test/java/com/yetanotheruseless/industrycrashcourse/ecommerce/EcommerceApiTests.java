fa6aeaf8640f911e8a7de7a2bdd0776f0660babd
lkeitadk <34991293+lkeitadk@users.noreply.github.com>
2021-04-07 21:01:35 -0600

    Added Visitor Entity (#56)
    * Added Visitor Entity
    
    * Fixed minor line to allow tests to pass
    
    * Other attempt to make test pass by fixing MockBean

--- ecommerce/src/test/java/com/yetanotheruseless/industrycrashcourse/ecommerce/EcommerceApiTests.java
+++ ecommerce/src/test/java/com/yetanotheruseless/industrycrashcourse/ecommerce/EcommerceApiTests.java
@@ -2,6 +2,9 @@ package com.yetanotheruseless.industrycrashcourse.ecommerce;
 
 import com.yetanotheruseless.industrycrashcourse.ecommerce.product.Product;
 import com.yetanotheruseless.industrycrashcourse.ecommerce.product.ProductService;
+import com.yetanotheruseless.industrycrashcourse.ecommerce.visitor.Visitor;
+import com.yetanotheruseless.industrycrashcourse.ecommerce.visitor.VisitorController;
+import com.yetanotheruseless.industrycrashcourse.ecommerce.visitor.VisitorService;
 import com.yetanotheruseless.industrycrashcourse.ecommerce.shoppingCart.ShoppingCart;
 import com.yetanotheruseless.industrycrashcourse.ecommerce.shoppingCart.ShoppingCartService;
 import org.junit.jupiter.api.Test;
@@ -13,8 +16,11 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.Instant;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Optional;
 
 import static org.hamcrest.core.StringContains.containsString;
+import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -32,16 +38,30 @@ public class EcommerceApiTests {
     private ProductService productService;
 
     @MockBean
+    private VisitorService visitorService;
+
+    @MockBean
     private ShoppingCartService shoppingCartService;
 
     @Test
     public void listAllTest() throws Exception {
         Product dummyFuji = new Product();
         dummyFuji.setName("Fuji");
-        when(productService.findAll()).thenReturn(Arrays.asList(dummyFuji));
+        dummyFuji.setSku("1234");
+        when(productService.findAll()).thenReturn(Collections.singletonList(dummyFuji));
 
         mockMvc
                 .perform(get("/api/v1/product/all"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Fuji")));
+
+        reset(productService);
+
+        when(productService.findBySku("1234")).thenReturn(Optional.of(dummyFuji));
+
+        mockMvc
+                .perform(get("/api/v1/product/1234"))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().string(containsString("Fuji")));
@@ -53,9 +73,32 @@ public class EcommerceApiTests {
                 .perform(get("/api/v1/product/foo/bar/baz"))
                 .andDo(print())
                 .andExpect(status().isNotFound());
-               // .andExpect(content().string(containsString("dude")));
+        // .andExpect(content().string(containsString("dude")));
     }
 
+    @Test
+    public void testVisitor() throws Exception {
+        Visitor visitor = new Visitor();
+        visitor.setId(12345L);
+        visitor.setSurname("Keita");
+        when(visitorService.findAll()).thenReturn(Collections.singletonList(visitor));
+        mockMvc
+                .perform(get("/api/v1/visitor/list"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Keita")));
+
+        reset(visitorService);
+
+        when(visitorService.findById(12345L)).thenReturn(Optional.of(visitor));
+
+        mockMvc
+                .perform(get("/api/v1/visitor/12345"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Keita")));
+    }
+    
     @Test
     public void testShoppingCartCreation() throws Exception {
         ShoppingCart cart = new ShoppingCart();
@@ -90,3 +133,4 @@ public class EcommerceApiTests {
     }
 
 }
+
